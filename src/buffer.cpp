b2ed3b6576c5156a330cbd7f7668d79ed030de68
Aidan Monty <amonty@wisc.edu>
2021-11-14 17:21:47 -0600

    final answer

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -44,9 +44,6 @@ namespace badgerdb
     clockHand = bufs - 1;
   }
 
-  /**
- * Advance clock to next frame in the buffer pool.
- */
   void BufMgr::advanceClock()
   {
     if (clockHand + 1 >= numBufs)
@@ -59,42 +56,25 @@ namespace badgerdb
     }
   }
 
-  /**
- * Allocates a free frame using the clock algorithm; if necessary, writing 
- * a dirty page back to disk. Throws BufferExceededException if all buffer 
- * frames are pinned. This private method will get called by the readPage()
- * and allocPage() methods described below. Make sure that if the buffer
- * frame allocated has a valid page in it, you remove the appropriate entry
- * from the hash table.
- */
   void BufMgr::allocBuf(FrameId &frame)
   {
-    std::cout << "        BufMgr: Starting allocBuf! \n";
-    // store current frame so that we can check if it repeats itself
-    // FrameId curFrame = clockHand;
-
-    // clock algorithm until it gets to the clock hand
     advanceClock();
-    std::cout << "        BufMgr: Advanced the clock to " << clockHand << ".\n";
-
     bool allocated = false;
-    // buffer exceeded exception only called if every frame is pinned
-    // we are allowed to cycle through the buffer pool more than once
-    // that said, not sure if this implementation is optimal
     std::vector<bool> pinned(numBufs, false);
-    int numPinned = 0;
-    while (!allocated && numPinned < numBufs)
+    uint32_t numPinned = 0;
+    while (!allocated)
     {
+      if (numPinned >= numBufs)
+      {
+        throw BufferExceededException();
+      }
       if (bufDescTable[clockHand].valid) {
-        // if the refbit is 1, reset it & advance the clock
         if (bufDescTable[clockHand].refbit)
         {
           bufDescTable[clockHand].refbit = false;
           advanceClock();
-          std::cout << "        BufMgr: Refbit was 1, so it was reset and the clock was advanced to " << clockHand << ".\n";
           continue;
         }
-        // if it's being used, skip it.
         else if (bufDescTable[clockHand].pinCnt > 0)
         {
           if (!pinned[clockHand]) {
@@ -102,41 +82,16 @@ namespace badgerdb
             numPinned++;
           }
           advanceClock();
-          std::cout << "        BufMgr: PinCnt > 0, clock advanced to " << clockHand << ".\n";
           continue;
         }
         else if (bufDescTable[clockHand].dirty)
         {
-          std::cout << "        BufMgr: Frame was dirty, starting to flush.\n";
-
-          // instructions say to flush page, not file:
-          // flushFile(bufDescTable[clockHand].file);
           bufDescTable[clockHand].file.writePage(bufPool[clockHand]);
         }
-        // replacing frame with valid page, so remove from hashtable
-        // try {
-        std::cout << "        Removing " << bufDescTable[clockHand].file.filename() << ", ";
-        std::cout << bufDescTable[clockHand].pageNo << " from hash table\n";
         hashTable.remove(bufDescTable[clockHand].file, bufDescTable[clockHand].pageNo);
-        // } catch (HashNotFoundException e) {
-        //   std::cout << "TODO : EVALUATE THIS CASE";
-        // }
       } 
-      
         frame = bufDescTable[clockHand].frameNo;
-        std::cout << "        BufMgr: FrameId set to " << frame << ".\n";
-      
-        // clear + set frame + end
-        // NOTE:    not sure if clearing is necessary
-        // std::cout << "        BufMgr: Starting to clear frame.\n";
-       // bufDescTable[clockHand].clear();
-        bufDescTable[clockHand].Print();
         allocated = true;
-    }
-
-    if (!allocated && numPinned >= numBufs)
-    {
-      throw new BufferExceededException();
     }
   }
 
@@ -150,39 +105,18 @@ namespace badgerdb
     {
       hashTable.lookup(file, pageNo, f);
       // page is in the buffer pool:
-
-      // set the refbit to true
       bufDescTable[f].refbit = true;
-
-      // increment pinCnt
       bufDescTable[f].pinCnt += 1;
-
-      // return a pointer to the frame containing the page via page parameter
       page = &bufPool[f];
     }
-    // page is not in the buffer pool:
-    catch (HashNotFoundException e)
+    catch (const HashNotFoundException &e)
     {
-      // might throw InvalidPageException if bad file
+      // page is not in the buffer pool:
       Page p = file.readPage(pageNo);
-      // call allocBuf
-      // try {
       allocBuf(f);
-      // } catch (BufferExceededException e) {
-      //   std::cout << "TODO : EVALUATE THIS CASE";
-      // }
       bufPool[f] = p;
-
-      std::cout << "        Adding " << file.filename() << ", ";
-      std::cout << pageNo << " to hash table at ";
-      std::cout << "frame " << f << "\n";
-      // insert page into hashtable
       hashTable.insert(file, pageNo, f);
-
-      // set() the frame to set it up properly
       bufDescTable[f].Set(file, pageNo);
-
-      // return pointer to frame containing the page via page parameter
       page = &bufPool[f];
     }
   }
@@ -194,7 +128,7 @@ namespace badgerdb
     {
       hashTable.lookup(file, pageNo, fid);
     }
-    catch (HashNotFoundException e)
+    catch (const HashNotFoundException &e)
     {
       std::cerr << e.message();
       return;
@@ -215,75 +149,45 @@ namespace badgerdb
 
   void BufMgr::allocPage(File &file, PageId &pageNo, Page* &page)
   {
-
-    std::cout << "        BufMgr: Starting allocPage! \n";
     FrameId fid;
-    // try {
     allocBuf(fid);
-    // } catch (const BufferExceededException &e) {
-    //   std::cout << "TODO : EVALUATE THIS CASE";
-    // }
-    
-    std::cout << "        BufMgr: allocBuf(fid) \n";
-
     bufPool[fid] = file.allocatePage();
-    std::cout << "        BufMgr: file.allocatePage() \n";
     page = &bufPool[fid];
     pageNo = bufPool[fid].page_number();
-    std::cout << "        Adding " << bufDescTable[fid].file.filename() << ", ";
-    std::cout << bufDescTable[fid].pageNo << " to hash table at ";
-    std::cout << "frame " << fid << "\n";
     hashTable.insert(file, pageNo, fid);
-    bufDescTable[clockHand].Print();
-    std::cout << "        BufMgr: inserted to hash table \n";
-
     bufDescTable[fid].Set(file, pageNo);
-    bufDescTable[clockHand].Print();
-    std::cout << "        BufMgr: set within bufDescTable \n";
-    
   }
 
   void BufMgr::flushFile(File &file)
   {
-    // TODO : need to check if pages are valid ?
-    // I did this function backwards, by iterating through every
-    // page in the file, then checking if that page was in the buffer
-    // pool. Not sure what implications this has (i.e. running time)
-    FileIterator fi = file.begin();
-    FileIterator end = file.end();
-    while (fi != end)
-    {
-      FrameId fid;
-      PageId pageNo = (*fi).page_number();
-      try
-      {
-        hashTable.lookup(file, pageNo, fid);
+    for (FrameId i = 0; i < numBufs; i++) {
+      if (bufDescTable[i].file==file) {
+        if (!bufDescTable[i].valid) {
+          throw BadBufferException(i, bufDescTable[i].dirty, bufDescTable[i].valid, bufDescTable[i].refbit);
+        }
+        if (bufDescTable[i].pinCnt > 0) {
+           PagePinnedException(file.filename(), bufDescTable[i].pageNo, i);
+        }
+        if (bufDescTable[i].dirty) {
+          file.writePage(bufPool[i]);
+          bufDescTable[i].dirty = false;
+        }
+        hashTable.remove(file, bufDescTable[i].pageNo);
+        bufDescTable[i].clear();
       }
-      catch (HashNotFoundException e)
-      {
-        fi++;
-        continue;
-      }
-      if (bufDescTable[fid].dirty)
-      {
-        file.writePage(pageNo, bufPool[fid]); // perform checks mentioned in method header ?
-        bufDescTable[fid].dirty = false;
-      }
-      hashTable.remove(file, pageNo);
-      bufDescTable[fid].clear();
     }
   }
 
   void BufMgr::disposePage(File &file, const PageId PageNo)
   {
-    // TODO : need to check if pinned ?
+    
     FrameId fid;
     bool frameAllocated = true;
     try
     {
       hashTable.lookup(file, PageNo, fid);
     }
-    catch (HashNotFoundException e)
+    catch (const HashNotFoundException &e)
     {
       frameAllocated = false;
     }
